3162983a77a8fc3cbcc9f7f3159a1bec1225a1ac
Leo Lark <leo.lark10@gmail.com>
2022-11-07 21:31:18 +1000

    4: formatting and flake8

--- test/test_selector.py
+++ test/test_selector.py
@@ -29,6 +29,7 @@ def initialize_resource_repo(dest, resource_repo):
     repo.git.commit("-m", "Initial commit")
     return dest
 
+
 @pytest.fixture
 def small_project_a(empty_git_repo):
     return initialize_resource_repo(empty_git_repo, "small_project_a")
@@ -64,7 +65,9 @@ def delete_f_small_project_a(project_root_dir):
 
 def add_h_small_project_a(project_root_dir):
     repo = git.Repo(project_root_dir)
-    repo.git.checkout("-b", "base")  # So we don't have to deduce whether it is main/master branch
+    repo.git.checkout(
+        "-b", "base"
+    )  # So we don't have to deduce whether it is main/master branch
     repo.git.checkout("-b", "feature")
     with open(os.path.join(project_root_dir, "small_project_a", "h.py"), "w+") as h:
         h.write("pass\n")
@@ -75,7 +78,7 @@ def add_h_small_project_a(project_root_dir):
         h.write("import small_project_a.h\n")
     repo.git.add(".")
     repo.git.commit("-m", "Add test_h.py")
-    
+
     with open(os.path.join(project_root_dir, "small_project_a", "g.py"), "a+") as g:
         g.write("import small_project_a.h")
     repo.git.add(".")
@@ -84,20 +87,20 @@ def add_h_small_project_a(project_root_dir):
 
 def modify_b_1_medium_project_a(project_root_dir):
     repo = git.Repo(project_root_dir)
-    
+
     with open(os.path.join(project_root_dir, "src", "b", "b_1.py"), "a+") as b_1:
         b_1.write("# modify_b_1_medium_project_a\n")
-    
+
     repo.git.add(".")
     repo.git.commit("-m", "Modify b_1.py")
 
 
 def b_2_depends_on_a_2_medium_project_a(project_root_dir):
     repo = git.Repo(project_root_dir)
-    
+
     with open(os.path.join(project_root_dir, "src", "b", "b_2.py"), "a+") as b_2:
         b_2.write("import a.a_2\n")
-    
+
     repo.git.add(".")
     repo.git.commit("-m", "Make b_2.py depend on a_2.py")
 
@@ -110,7 +113,9 @@ def b_2_depends_on_a_2_medium_project_a(project_root_dir):
 
 def complex_workflow_a_medium_project_a(project_root_dir):
     repo = git.Repo(project_root_dir)
-    repo.git.checkout("-b", "base")  # So we don't have to deduce whether it is main/master branch
+    repo.git.checkout(
+        "-b", "base"
+    )  # So we don't have to deduce whether it is main/master branch
     repo.git.checkout("-b", "feature-1")
 
     with open(os.path.join(project_root_dir, "src", "a", "a_1.py"), "a+") as a_1:
@@ -133,7 +138,6 @@ def complex_workflow_a_medium_project_a(project_root_dir):
     with open(os.path.join(project_root_dir, "src", "b", "b_1.py"), "a+") as b_1:
         b_1.write("# b_1.py complex_workflow_a_medium_project_a\n")
 
-    
     repo.git.add(".")
     repo.git.commit("-m", "Modify b_1.py")
 
@@ -142,55 +146,114 @@ def complex_workflow_a_medium_project_a(project_root_dir):
     repo.git.add(".")
     repo.git.commit("-m", "Delete a_2.py")
 
+
 def complex_workflow_a_medium_project_a_feature_1(project_root_dir):
     complex_workflow_a_medium_project_a(project_root_dir)
     git.Repo(project_root_dir).git.checkout("feature-1")
-    
+
 
 @pytest.mark.parametrize(
     ("repo", "side_effect", "git_diff_args", "test_paths", "python_paths", "expected"),
     [
-        ("small_project_a", modify_f_small_project_a, ["HEAD~1..."], ["test"], ["."], {"test/test_f.py", "test/test_g.py"}),
-        ("small_project_a", modify_g_small_project_a, ["HEAD~1..."], ["test"], ["."], {"test/test_g.py"}),
-        ("small_project_a", delete_f_small_project_a, ["HEAD~1..."], ["test"], ["."], {"test/test_f.py", "test/test_g.py"}),
-        ("small_project_a", add_h_small_project_a, ["base..."], ["test"], ["."], {"test/test_g.py", "test/test_h.py"}),
-        ("medium_project_a", modify_b_1_medium_project_a, ["HEAD~1..."], ["test"], ["src"], {
-            "test/test_a/test_a_3.py", 
-            "test/test_b/test_b_1.py", 
-            "test/test_b/test_b.py", 
-            "test/test_c/test_c_1.py",
-            "test/test_d/test_d_1.py",  
-        }),
-        ("medium_project_a", b_2_depends_on_a_2_medium_project_a, ["HEAD~2..."], ["test"], ["src"], {
-            "test/test_a/test_a_2.py", 
-            "test/test_a/test_a_3.py", 
-            "test/test_b/test_b_2.py", 
-            "test/test_b/test_b.py", 
-            "test/test_c/test_c_1.py",
-            "test/test_c/test_c_2.py",
-            "test/test_d/test_d_1.py",  
-        }),
-        ("medium_project_a", complex_workflow_a_medium_project_a, ["base..."], ["test"], ["src"], {
-            "test/test_a/test_a_1.py", 
-            "test/test_a/test_a_2.py", 
-            "test/test_a/test_a_3.py", 
-            "test/test_b/test_b_1.py", 
-            "test/test_b/test_b.py", 
-            "test/test_c/test_c_1.py",
-            "test/test_c/test_c_2.py",
-            "test/test_d/test_d_1.py",  
-        }),
-        ("medium_project_a", complex_workflow_a_medium_project_a_feature_1, ["base..."], ["test"], ["src"], {
-            "test/test_a/test_a_1.py", 
-            "test/test_a/test_a_3.py", 
-            "test/test_b/test_b_2.py", 
-            "test/test_b/test_b.py", 
-            "test/test_c/test_c_1.py",
-            "test/test_d/test_d_1.py",  
-        }),
+        (
+            "small_project_a",
+            modify_f_small_project_a,
+            ["HEAD~1..."],
+            ["test"],
+            ["."],
+            {"test/test_f.py", "test/test_g.py"},
+        ),
+        (
+            "small_project_a",
+            modify_g_small_project_a,
+            ["HEAD~1..."],
+            ["test"],
+            ["."],
+            {"test/test_g.py"},
+        ),
+        (
+            "small_project_a",
+            delete_f_small_project_a,
+            ["HEAD~1..."],
+            ["test"],
+            ["."],
+            {"test/test_f.py", "test/test_g.py"},
+        ),
+        (
+            "small_project_a",
+            add_h_small_project_a,
+            ["base..."],
+            ["test"],
+            ["."],
+            {"test/test_g.py", "test/test_h.py"},
+        ),
+        (
+            "medium_project_a",
+            modify_b_1_medium_project_a,
+            ["HEAD~1..."],
+            ["test"],
+            ["src"],
+            {
+                "test/test_a/test_a_3.py",
+                "test/test_b/test_b_1.py",
+                "test/test_b/test_b.py",
+                "test/test_c/test_c_1.py",
+                "test/test_d/test_d_1.py",
+            },
+        ),
+        (
+            "medium_project_a",
+            b_2_depends_on_a_2_medium_project_a,
+            ["HEAD~2..."],
+            ["test"],
+            ["src"],
+            {
+                "test/test_a/test_a_2.py",
+                "test/test_a/test_a_3.py",
+                "test/test_b/test_b_2.py",
+                "test/test_b/test_b.py",
+                "test/test_c/test_c_1.py",
+                "test/test_c/test_c_2.py",
+                "test/test_d/test_d_1.py",
+            },
+        ),
+        (
+            "medium_project_a",
+            complex_workflow_a_medium_project_a,
+            ["base..."],
+            ["test"],
+            ["src"],
+            {
+                "test/test_a/test_a_1.py",
+                "test/test_a/test_a_2.py",
+                "test/test_a/test_a_3.py",
+                "test/test_b/test_b_1.py",
+                "test/test_b/test_b.py",
+                "test/test_c/test_c_1.py",
+                "test/test_c/test_c_2.py",
+                "test/test_d/test_d_1.py",
+            },
+        ),
+        (
+            "medium_project_a",
+            complex_workflow_a_medium_project_a_feature_1,
+            ["base..."],
+            ["test"],
+            ["src"],
+            {
+                "test/test_a/test_a_1.py",
+                "test/test_a/test_a_3.py",
+                "test/test_b/test_b_2.py",
+                "test/test_b/test_b.py",
+                "test/test_c/test_c_1.py",
+                "test/test_d/test_d_1.py",
+            },
+        ),
     ],
 )
-def test_select_test_files(repo, side_effect, git_diff_args, test_paths, python_paths, expected, request):
+def test_select_test_files(
+    repo, side_effect, git_diff_args, test_paths, python_paths, expected, request
+):
     repo_path = request.getfixturevalue(repo)
     side_effect(repo_path)
 
