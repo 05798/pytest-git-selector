7f0ede22f098d1d413015d4bb6a0c2f884860dba
Leo Lark <leo.lark10@gmail.com>
2022-11-08 14:09:12 +1000

    #9: add src-paths pytest arg to improve perf

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -9,19 +9,21 @@ from conftest import (
 
 
 @pytest.mark.parametrize(
-    ("repo", "side_effect", "git_diff_args", "expected_outcomes"),
+    ("repo", "side_effect", "git_diff_args", "src_path", "expected_outcomes"),
     [
-        ("small_project_a", modify_f_small_project_a, ["HEAD~1"], {"passed": 2}),
+        ("small_project_a", modify_f_small_project_a, ["HEAD~1"], ["."], {"passed": 2}),
         (
             "small_project_a",
             modify_g_small_project_a,
             ["HEAD~1"],
+            ["."], 
             {"passed": 1, "deselected": 1},
         ),
         (
             "medium_project_a",
             complex_workflow_a_medium_project_a,
             ["base..."],
+            ["src"], 
             {
                 "deselected": 1,
                 "errors": 2,
@@ -29,7 +31,7 @@ from conftest import (
         ),
     ],
 )
-def test_plugin(repo, side_effect, git_diff_args, expected_outcomes, pytester, request):
+def test_plugin(repo, side_effect, git_diff_args, src_path, expected_outcomes, pytester, request):
     repo_path = request.getfixturevalue(repo)
     os.chdir(repo_path)
     side_effect(repo_path)
@@ -37,6 +39,9 @@ def test_plugin(repo, side_effect, git_diff_args, expected_outcomes, pytester, r
     pytester.syspathinsert(repo_path)
     pytester.syspathinsert(os.path.join(repo_path, "src/"))
 
-    result = pytester.runpytest("--git-diff-args", *git_diff_args)
+    src_path = [os.path.join(repo_path, p) for p in src_path]
+    src_path_args = sum(zip(["--src-path"] * len(src_path), src_path), ())
+
+    result = pytester.runpytest(*(list(src_path_args) + ["--git-diff-args"] + git_diff_args))
 
     result.assert_outcomes(**expected_outcomes)
