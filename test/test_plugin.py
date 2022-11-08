f4515a9573d2cf62cd45cbbaaa31c63ed81d3d3e
Leo Lark <leo.lark10@gmail.com>
2022-11-08 12:27:27 +1000

    #9: black formatting

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -11,25 +11,23 @@ from conftest import (
 @pytest.mark.parametrize(
     ("repo", "side_effect", "git_diff_args", "expected_outcomes"),
     [
+        ("small_project_a", modify_f_small_project_a, ["HEAD~1"], {"passed": 2}),
         (
-            "small_project_a", 
-            modify_f_small_project_a, 
-            ["HEAD~1"], 
-            {"passed": 2}
-        ), 
-        (
-            "small_project_a", 
-            modify_g_small_project_a, 
-            ["HEAD~1"], 
-            {"passed": 1, "deselected": 1}
-        ), 
+            "small_project_a",
+            modify_g_small_project_a,
+            ["HEAD~1"],
+            {"passed": 1, "deselected": 1},
+        ),
         (
             "medium_project_a",
             complex_workflow_a_medium_project_a,
             ["base..."],
-            {"deselected": 1, "errors": 2},  # errors since the workflow deletes a source file without changing imports
+            {
+                "deselected": 1,
+                "errors": 2,
+            },  # errors since the workflow deletes a source file without changing imports
         ),
-    ]
+    ],
 )
 def test_plugin(repo, side_effect, git_diff_args, expected_outcomes, pytester, request):
     repo_path = request.getfixturevalue(repo)
@@ -37,7 +35,7 @@ def test_plugin(repo, side_effect, git_diff_args, expected_outcomes, pytester, r
     side_effect(repo_path)
 
     pytester.syspathinsert(repo_path)
-    pytester.syspathinsert(os.path.join(repo_path, 'src/'))
+    pytester.syspathinsert(os.path.join(repo_path, "src/"))
 
     result = pytester.runpytest("--git-diff-args", *git_diff_args)
 
