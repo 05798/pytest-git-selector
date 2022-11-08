6adf053b2992a7ba4e768a0387dcbe30b7a03142
Leo Lark <leo.lark10@gmail.com>
2022-11-08 15:03:34 +1000

    #11: use argparse.REMAINDER for --git-diff-args

--- src/pytest_git_selector/plugin.py
+++ src/pytest_git_selector/plugin.py
@@ -1,16 +1,22 @@
+import argparse
 import pytest
 import pytest_git_selector.selector
 
 
 def pytest_addoption(parser):
     group = parser.getgroup("pytest-git-selector")
-    group.addoption("--git-diff-args", nargs="*", help="args to pass to git diff")
     group.addoption(
         "--src-path",
         action="append",
-        help="args to pass to git diff",
+        help=(
+            "args to pass to git diff. "
+            "git diff is called internally with the --name-only and --no-renames automatically. "
+            "Any additional arguments must not interfere with the output format e.g. do not use the --output flag " 
+            "which writes to a file instead of stdout"
+        ), 
         default=[".", "./src/"],
     )
+    group.addoption("--git-diff-args", nargs=argparse.REMAINDER, help="args to pass to git diff")
 
 
 @pytest.hookimpl()
