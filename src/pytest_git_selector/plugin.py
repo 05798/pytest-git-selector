4a1a796273daca4fc90063fe21c62cf7c668dee7
Leo Lark <leo.lark10@gmail.com>
2022-11-08 12:03:32 +1000

    #9: pytest plugin

---
+++ src/pytest_git_selector/plugin.py
@@ -0,0 +1,32 @@
+import pytest
+import pytest_git_selector.selector
+import sys
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("pytest-git-selector")
+    group.addoption(
+        "--git-diff-args", 
+        nargs="*", 
+        help="args to pass to git diff"
+    )
+
+
+@pytest.hookimpl()
+def pytest_collection_modifyitems(session, config, items):
+    all_test_files = list(set(str(item.path) for item in items))  # remove duplicates
+    git_diff_args = config.getoption("--git-diff-args")
+
+    if git_diff_args:
+        selected_items = pytest_git_selector.selector.select_test_files(
+            git_diff_args, 
+            all_test_files, 
+            sys.path,  # Invoking interpreter should have the repository src in its PYTHONPATH
+            str(session.startpath)
+        )
+
+        deselected = [item for item in session.items if str(item.path) not in selected_items]
+        config.hook.pytest_deselected(items=deselected)
+
+        # Docs say this should be done in-place
+        items[:] = [item for item in session.items if str(item.path) in selected_items]
