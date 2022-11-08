1e3406c6606f8cc178580dc95038847b9cd70449
Leo Lark <leo.lark10@gmail.com>
2022-11-09 01:10:56 +1000

    #7: accept arbitrary extra deps

--- src/pytest_git_selector/selector.py
+++ src/pytest_git_selector/selector.py
@@ -6,7 +6,7 @@ import sys
 from importlab import environment
 from importlab import graph
 from importlab import utils
-from typing import List, MutableSet
+from typing import List, MutableSet, Optional, Tuple
 
 
 def select_test_files(
@@ -14,32 +14,50 @@ def select_test_files(
     test_paths: List[str],
     python_path: List[str],
     dir_name: str = ".",
+    extra_deps: Optional[List[Tuple[str, str]]] = None,
 ) -> MutableSet[str]:
     repo = git.Repo(dir_name)
 
     # Use --no-renames treats renames as an deletion of the pre-rename file and addition of the post-rename file
     # This is easier to deal with in terms of analyzing the dependencies
-    diff_files = repo.git.diff("--name-only", "--no-renames", *git_diff_args).split(
+    diff_files_relative_path = repo.git.diff("--name-only", "--no-renames", *git_diff_args).split(
         "\n"
     )
-    py_diff_files = utils.expand_source_files(
-        set(f for f in diff_files if f.endswith(".py")), dir_name
-    )
+    diff_files = [os.path.join(dir_name, p) for p in diff_files_relative_path]
+
     import_graph = _create_import_graph(
         git_diff_args, python_path, test_paths=test_paths, dir_name=dir_name
     )
+
+    if extra_deps:
+        extra_deps = _to_absolute_path_extra_deps(extra_deps, dir_name)
+        import_graph.graph.add_edges_from(extra_deps)
 
     root_ancestor_nodes = set()
 
     for node in import_graph.graph.nodes:
         filename = import_graph.format(node)
 
-        if filename in py_diff_files:
+        if filename in diff_files:
             _find_root_ancestors(import_graph.graph, node, root_ancestor_nodes)
 
     root_ancestor_nodes = set(map(import_graph.format, root_ancestor_nodes))
 
     return root_ancestor_nodes
+
+
+def _to_absolute_path_extra_deps(extra_deps: List[Tuple[str, str]], base_dir_name: str):
+    extra_deps_absolute_paths = []
+
+    for u, v in extra_deps:
+        extra_deps_absolute_paths.append(
+            (
+                u if os.path.isabs(u) else os.path.join(base_dir_name, u), 
+                v if os.path.isabs(v) else os.path.join(base_dir_name, v)
+            )
+        )
+
+    return extra_deps_absolute_paths
 
 
 def _create_import_graph(
