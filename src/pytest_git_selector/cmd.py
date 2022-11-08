cc345e1170d959152dba9da45e1abb51527acc2f
Leo Lark <leo.lark10@gmail.com>
2022-11-08 13:16:36 +1000

    #9: simplify command line argument handling

--- src/pytest_git_selector/cmd.py
+++ src/pytest_git_selector/cmd.py
@@ -6,22 +6,20 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--dir",
-        help="Base directory of the project containing the .git folder",
+        help="base directory of the project containing the .git folder",
         default=".",
     )
     parser.add_argument(
         "--test-path",
-        help="Path of a test file or directory containing test files",
+        help="path of a test file or directory containing test files. These are relative to the current working directory not the directory specified in '--dir'",
         required=True,
-        action="append",
-        nargs=1,
+        action="append"
     )
     parser.add_argument(
         "--src-path",
-        help="Path of directory containing source files for the project",
+        help="path of directory containing source files for the project. These are relative to the current working directory not the directory specified in '--dir'",
         required=True,
-        action="append",
-        nargs=1,
+        action="append"
     )
     parser.add_argument(
         "subargs",
@@ -35,7 +33,7 @@ def main():
     args = parse_args()
 
     required_test_files = pytest_git_selector.selector.select_test_files(
-        args.subargs, sum(args.test_path, []), sum(args.src_path, []), dir_name=args.dir
+        args.subargs, args.test_path, args.src_path, dir_name=args.dir
     )
 
     print("\n".join(required_test_files))
