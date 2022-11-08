30492c5472919c879cf77600b1903475e9376dc9
Leo Lark <leo.lark10@gmail.com>
2022-11-09 02:33:36 +1000

    #7: --extra-deps-file in pytest plugin

--- src/pytest_git_selector/plugin.py
+++ src/pytest_git_selector/plugin.py
@@ -1,6 +1,7 @@
 import argparse
 import pytest
 import pytest_git_selector.selector
+import pytest_git_selector.util
 
 
 def pytest_addoption(parser):
@@ -17,6 +18,16 @@ def pytest_addoption(parser):
         default=[".", "./src/"],
     )
     group.addoption(
+        "--extra-deps-file",
+        help=(
+            "path of a file specifying extra module dependencies. "
+            "Edges should be in the form '(a.py,b.py)' where a.py imports b.py. Edges separated by a space or newline. "
+            "NOTE there is NO space after the comma. If edges are specified using relative paths, they interpreted as "
+            "being relative to the directory containing the project root directory containing the .git folder."
+        ),
+        default=None,
+    )
+    group.addoption(
         "--git-diff-args", nargs=argparse.REMAINDER, help="args to pass to git diff"
     )
 
@@ -24,14 +35,21 @@ def pytest_addoption(parser):
 @pytest.hookimpl()
 def pytest_collection_modifyitems(session, config, items):
     all_test_files = list(set(str(item.path) for item in items))  # remove duplicates
-    git_diff_args = config.getoption("--git-diff-args")
 
-    if git_diff_args:
+    if git_diff_args := config.getoption("--git-diff-args"):
+        if extra_deps_filename := config.getoption("--extra-deps-file"):
+            extra_deps = pytest_git_selector.util.parse_extra_deps_file(
+                extra_deps_filename
+            )
+        else:
+            extra_deps = None
+
         selected_items = pytest_git_selector.selector.select_test_files(
             git_diff_args,
             all_test_files,
             config.getoption("--src-path"),
             str(session.startpath),
+            extra_deps=extra_deps,
         )
 
         deselected = [
