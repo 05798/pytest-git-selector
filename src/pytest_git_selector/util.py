ed15be03ebabe20bc992c56b823a04c6451f501c
Leo Lark <leo.lark10@gmail.com>
2022-11-09 02:15:24 +1000

    #7: better path handling

---
+++ src/pytest_git_selector/util.py
@@ -0,0 +1,17 @@
+import pathlib
+import os
+from typing import List, Tuple
+
+
+def parse_extra_deps_file(extra_deps_filename) -> List[Tuple[str, str]]:
+    with open(extra_deps_filename, "r") as f:
+        edges = []
+        for line in f:
+            # Edges should be whitespace separated in the form (a,b)
+            edges.extend(tuple(e.strip("()").split(",")) for e in line.split())
+
+    return edges
+
+
+def to_absolute_path(base_dir_name: str, relative_path: str) -> pathlib.Path:
+    return pathlib.Path(os.path.join(base_dir_name, relative_path))
