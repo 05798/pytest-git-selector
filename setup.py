c1ec0ba9285676d61d2895220638b7a5b6fc52bd
Leo Lark <leo.lark10@gmail.com>
2022-11-12 20:56:04 +1000

    #13: python-publish job

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+import os
+
 from setuptools import setup
 
 
 if __name__ == "__main__":
-    setup()
+    # Version is determined using the dunamai tool https://github.com/mtkennerly/dunamai
+
+    version = os.getenv("PYTEST_GIT_SELECTOR_VERSION", "")  # VERSION is set by the python-publish job
+
+    if not version:  # building locally
+        import dunamai
+        version = dunamai.Version.from_git()  
+
+    setup(
+        version=version
+    )
