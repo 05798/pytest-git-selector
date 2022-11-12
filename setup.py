35b2c83d075a4545e2a92d4688e749e2040d25de
Leo Lark <leo.lark10@gmail.com>
2022-11-12 21:04:12 +1000

    #13: remove dunamai as build req

--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ from setuptools import setup
 if __name__ == "__main__":
     # Version is determined using the dunamai tool https://github.com/mtkennerly/dunamai
 
-    version = os.getenv("PYTEST_GIT_SELECTOR_VERSION", "")  # VERSION is set by the python-publish job
+    version = os.getenv(
+        "PYTEST_GIT_SELECTOR_VERSION", ""
+    )  # VERSION is set by the python-publish job
 
     if not version:  # building locally
-        import dunamai
-        version = dunamai.Version.from_git()  
-
-    setup(
-        version=version
-    )
+        version = "0.0.0.dev0"
+    setup(version=version)
